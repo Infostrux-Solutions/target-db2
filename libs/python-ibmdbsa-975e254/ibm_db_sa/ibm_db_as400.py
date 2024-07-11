d9968c3ab8b97c4ea05aec09fc13f2286cbbe1c2
Haleemur Ali <haleemur@gmail.com>
2024-07-11 17:59:32 -0400

    add vendored ibm-db-sa

---
+++ libs/python-ibmdbsa-975e254/ibm_db_sa/ibm_db_as400.py
@@ -0,0 +1,13 @@
+from sqlalchemy import __version__ as SA_Version
+
+SA_Version = [int(ver_token) for ver_token in SA_Version.split(".")[0:2]]
+if SA_Version < [2, 0]:
+    pass
+else:
+    pass
+from ibm_db_sa.ibm_db import DB2Dialect_ibm_db
+from ibm_db_sa.reflection import AS400Reflector
+
+
+class AS400Dialect(DB2Dialect_ibm_db):
+    _reflector_cls = AS400Reflector
