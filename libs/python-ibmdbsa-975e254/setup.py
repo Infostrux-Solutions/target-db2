d2742f543e6acced3ba14f4a000e630545b50f0d
Haleemur Ali <haleemur@gmail.com>
2024-07-12 10:28:14 -0400

    fix dependency build due to bad regex

--- libs/python-ibmdbsa-975e254/setup.py
+++ libs/python-ibmdbsa-975e254/setup.py
@@ -5,10 +5,7 @@ import re
 
 from setuptools import setup
 
-v = open(os.path.join(os.path.dirname(__file__), "ibm_db_sa", "__init__.py"))
-VERSION = re.compile(r".*__version__ = '(.*?)'", re.DOTALL).match(v.read()).group(1)
-v.close()
-
+VERSION = "0.4.0"
 readme = os.path.join(os.path.dirname(__file__), "README.md")
 if "USE_PYODBC" in os.environ and os.environ["USE_PYODBC"] == "1":
     require = ["sqlalchemy>=0.7.3"]
